8c798e8360440cd547871a6a8fda71ceb387a9ed
Liyajoseph <>
2021-06-01 15:21:26 +0000

    .body1 {   background-color: antiquewhite; }
    #id1 {
      color: cornflowerblue;
      font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
      background-color: yellow;
    }

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// the button's functions
+// it listens to the click.
 document.getElementById('button1').addEventListener('click', home)
 
+// it lets the button know what to do after the click.
 function home () {
   alert('Hello, World')
+}
+
+// it listens to the click.
+document.getElementById('button2').addEventListener('click', home2)
+
+// it lets this button know what to do after the click.
+function home2 () {
+  document.getElementById('id1').style.color = 'red'
 }
